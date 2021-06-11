af946f106cb2e52564e897fb374c06f3ca3ee09f
Shogo_Ogura <noonogura1112@gmail.com>
2021-06-11 15:54:49 +0900

    First

---
+++ DirectX12 Game Solution/DirectX12 MainProject/MainScene.cpp
@@ -0,0 +1,132 @@
+//
+// MainScene.cpp
+//
+
+#include "Base/pch.h"
+#include "Base/dxtk.h"
+#include "SceneFactory.h"
+
+// Initialize member variables.
+MainScene::MainScene() : dx9GpuDescriptor{}
+{
+    //コンストラクター
+    //変数の初期化（0にする）
+
+}
+
+// Initialize a variable and audio resources.
+void MainScene::Initialize()
+{
+    //ゲームシーンの初期化
+    //変数を初期化（具体的な数値を設定する）
+    //サウンドを読み込んだりする
+
+
+}
+
+// Allocate all memory the Direct3D and Direct2D resources.
+void MainScene::LoadAssets()
+{
+    //グラフィックの設定をする
+    //Windows10はドライバがアップデートされると読み込み直しが必要になる
+
+    descriptorHeap = DX12::CreateDescriptorHeap(DXTK->Device, 1);
+
+    ResourceUploadBatch resourceUploadBatch(DXTK->Device);
+    resourceUploadBatch.Begin();
+
+    RenderTargetState rtState(DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_FORMAT_D32_FLOAT);
+    SpriteBatchPipelineStateDescription pd(rtState, &CommonStates::NonPremultiplied);
+    D3D12_VIEWPORT viewport = {
+        0.0f, 0.0f, 1280.0f, 720.0f,
+        D3D12_MIN_DEPTH, D3D12_MAX_DEPTH
+    };
+
+    
+
+    spriteBatch = DX12::CreateSpriteBatch(DXTK->Device, resourceUploadBatch, pd, &viewport);
+
+    dx9GpuDescriptor = DXTK->Direct3D9->CreateShaderResourceView(descriptorHeap.get(), 0);
+
+    auto uploadResourcesFinished = resourceUploadBatch.End(DXTK->CommandQueue);
+    uploadResourcesFinished.wait();
+
+
+    // グラフィックリソースの初期化処理
+}
+
+// Releasing resources required for termination.
+void MainScene::Terminate()
+{
+    DXTK->ResetAudioEngine();
+    DXTK->WaitForGpu();
+
+    // TODO: Add your Termination logic here.
+    //ゲーム(シーン)終了時に何かしたい場合に使う
+}
+
+// Direct3D resource cleanup.
+void MainScene::OnDeviceLost()
+{
+    //ドライバーアップデートを検出した場合に使う
+    //読み込みなおす前に何かしたい場合に何か書く
+}
+
+// Restart any looped sounds here
+void MainScene::OnRestartSound()
+{
+    //サウンドデバイスが切り替わるとサウンドが全部停止する
+    //BGMなどを再スタートさせる
+}
+
+// Updates the scene.
+NextScene MainScene::Update(const float deltaTime)
+{
+    // If you use 'deltaTime', remove it.
+    UNREFERENCED_PARAMETER(deltaTime);
+
+    // TODO: Add your game logic here.
+    //ゲームを動かすプログラムを記述する
+
+    
+
+    return NextScene::Continue;
+}
+
+// Draws the scene.
+void MainScene::Render()
+{
+    // TODO: Add your rendering code here.
+    //描画のプログラムを書く
+    
+    DXTK->Direct3D9->Clear(DX9::Colors::RGBA(0, 0, 0, 255));  // 手順1
+
+    DXTK->Direct3D9->BeginScene();    // 手順2
+    DX9::SpriteBatch->Begin();        // 手順4
+
+    // (ここに2D描画の処理が入る)     // 手順5
+
+    
+
+    DX9::SpriteBatch->End();          // 手順6
+    DXTK->Direct3D9->EndScene();      // 手順7
+
+    DXTK->Direct3D9->UpdateResource();
+
+    DXTK->ResetCommand();
+    DXTK->ClearRenderTarget(Colors::CornflowerBlue);
+
+    const auto heapes = descriptorHeap->Heap();
+    DXTK->CommandList->SetDescriptorHeaps(1, &heapes);
+
+    spriteBatch->Begin(DXTK->CommandList);
+    spriteBatch->Draw(
+        dx9GpuDescriptor,
+        XMUINT2(1280, 720),
+        SimpleMath::Vector2(0.0f, 0.0f)
+    );
+    spriteBatch->End();
+
+    DXTK->Direct3D9->WaitUpdate();
+    DXTK->ExecuteCommandList();
+}
